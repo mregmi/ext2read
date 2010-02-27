2246690ab6f7f1966ccc9df6dfe2099fd1514857
Manish Regmi <regmi.manish@gmail.com>
2010-02-27 18:23:45 +0000

    - changed name to win-kvm - used new file/directory layout to be consistent with linux kernel layout.

---
+++ win-kvm/kvm-kmod-config.h
@@ -0,0 +1,1 @@
+#define KERNEL_EXTRAVERSION 0
---
+++ win-kvm/x86/.built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_/media/BACKUP/programming/contrib/kvmwin/kvm-kmod/x86/built-in.o :=  rm -f /media/BACKUP/programming/contrib/kvmwin/kvm-kmod/x86/built-in.o; ar rcs /media/BACKUP/programming/contrib/kvmwin/kvm-kmod/x86/built-in.o

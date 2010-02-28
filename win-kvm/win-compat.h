45eb87b4b9432329bb81207314e8bf05e2bba2b1
Manish Regmi <regmi.manish@gmail.com>
2010-02-28 17:58:19 +0000

    more header additions and cleanups

--- win-kvm/win-compat.h
+++ win-kvm/win-compat.h
@@ -12,13 +12,34 @@ typedef uint16_t __u16;
 typedef uint32_t __u32;
 typedef uint64_t __u64;
 
+typedef int8_t __s8;
+typedef int16_t __s16;
+typedef int32_t __s32;
+typedef int64_t __s64;
+
 typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
 
+typedef int8_t s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
+
+typedef int bool;
+
+//// ARCH DEPENDENT
+#ifdef WINKVM_32
+typedef  uint32_t size_t;
+#define PAGE_SHIFT	12
+#endif
+
 struct mutex {
 };
+
+typedef struct spinlock {
+}spinlock_t;
 
 struct kmem_cache {
 };
@@ -26,4 +47,5 @@ struct kmem_cache {
 typedef struct wait_queue {
 } wait_queue_head_t;
 
-#endif 
+#endif
+

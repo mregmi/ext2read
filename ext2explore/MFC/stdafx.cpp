f96569bede6816f67c1f7161dabf6f2446b8abfc
Manish Regmi <regmi.manish@gmail.com>
2010-03-13 15:27:26 +0000

    some directory restructuring

--- ext2explore/MFC/stdafx.cpp
+++
@@ -1,7 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// ext2explore.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
